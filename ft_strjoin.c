75606d22d7688d34e1f94cfc10c68781b69337ac
9Thanaphat <tangcham.thanaphat@gmail.com>
2025-01-26 10:00:51 +0700

    fixed ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -2,29 +2,25 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*strjoin;
 	int		i;
 	int		j;
+	char	*str;
 
-	strjoin = (char *) malloc (ft_strlen(s1) + ft_strlen(s2) * sizeof(char));
-	if (strjoin == NULL)
-		return (NULL);
 	i = 0;
 	j = 0;
-	while (s1[i])
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
 	{
-		strjoin[j] = s1[i];
+		str[i] = s1[i];
 		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
 		j++;
 	}
-	i = 0;
-	while (s2[i])
-	{
-		strjoin[j] = s2[i];
-		i++;
-		j++;
-	}
-	strjoin[j] = '\0';
-	i = 0;
-	return (strjoin);
+	str[i + j] = '\0';
+	return (str);
 }
